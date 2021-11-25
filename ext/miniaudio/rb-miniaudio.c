31030f0a91223a5cc45cec1a3b8c50ad81b1e904
Ivan Razuvaev <slowpilot@Ivans-MacBook-Pro.local>
2021-11-25 23:55:07 +0300

    declaring extension as ractor safe for ruby 3+

--- ext/miniaudio/rb-miniaudio.c
+++ ext/miniaudio/rb-miniaudio.c
@@ -4,6 +4,9 @@
 #include "convert_functions.h"
 
 void Init_miniaudio(void) {
+#ifdef HAVE_RUBY_RACTOR_H
+  rb_ext_ractor_safe(true);
+#endif
   VALUE rb_cMod = rb_const_get(rb_cObject, rb_intern("Miniaudio"));
   define_read_functions_under(rb_cMod);
   define_convert_functions_under(rb_cMod);
